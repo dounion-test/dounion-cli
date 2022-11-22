e387bfa014126f444921c7d479531b099a5ac7fe
hongye.wu <guobin.wang@dounion.com>
2022-11-22 18:11:16 +0800

    添加配置文件

---
+++ dl.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    notUpdate: {
+        files: [
+            'package.json',
+        ],  
+        folders: [
+            'src/views',
+            'src/api',
+        ]
+    }
+}
